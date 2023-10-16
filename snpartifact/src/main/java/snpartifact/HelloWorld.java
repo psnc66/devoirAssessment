1d182ff22e4889ac5a800eac04f51c54a1cff1a6
SainivasPaladugu <101536349+SainivasPaladugu@users.noreply.github.com>
2023-10-16 12:26:20 -0400

    Add files via upload

---
+++ snpartifact/src/main/java/snpartifact/HelloWorld.java
@@ -0,0 +1,11 @@
+package snpartifact;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello Devoir Employees");
+
+	}
+
+}
